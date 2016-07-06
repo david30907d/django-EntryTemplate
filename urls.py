1ad636407e0766c8e3b255f78fa189ebc43c8c3c
david <davidtnfsh@gmail.com>
2016-07-06 12:43:58 +0800

    將入口網站分成獨立的專案

---
+++ urls.py
@@ -0,0 +1,5 @@
+from django.conf.urls import patterns, url
+import apps.entry
+urlpatterns = patterns('apps.entry.views',
+  url(r'^$','entry'),#這樣做似乎是對應到首頁
+)
---
+++ views.py
@@ -0,0 +1,6 @@
+from django.http import HttpResponse
+from django.shortcuts import render_to_response
+
+def entry(request):
+		return render_to_response('entry/index.html',locals())
+
