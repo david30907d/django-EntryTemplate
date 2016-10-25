0e72509303ef9aa0f052b0dd0fa194e3903739e2
davidtnfsh <davidtnfsh@gmail.com>
2016-10-25 17:45:33 +0800

    fix django 1.9 url deprecated syntax, port it into django1.10

--- urls.py
+++ urls.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import patterns, url
-import entry
-urlpatterns = patterns('entry.views',
-  url(r'^$','entry', name='entry'),#這樣做似乎是對應到首頁
-)
+from entry import views
+urlpatterns = [
+  url(r'^$', views.entry, name='entry'),#這樣做似乎是對應到首頁
+]
